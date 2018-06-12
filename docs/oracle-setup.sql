1a42670a8abfa18708ef7a103c8e047de281d145
kkester <kkester@pivotal.io>
2018-06-12 16:52:09 -0500

    Updates to oracle-setup.sql to include function

--- docs/oracle-setup.sql
+++ docs/oracle-setup.sql
@@ -25,11 +25,29 @@ IS
   BEGIN
 
     email := upper(email);
-    id := DBMS_RANDOM.RANDOM;
+    id := dbms_random.value(100,9999);
     created_at := sysdate;
 
     insert into employee values (id, email, first_name, last_name, created_at);
 
   COMMIT;
   END;
+
+CREATE OR REPLACE FUNCTION employeeFunction(
+  first_name IN     varchar2,
+  last_name  IN     varchar2,
+  email      IN     varchar2,
+  created_at OUT    date)
+  RETURN number
+IS
+  id number;
+BEGIN
+    id := dbms_random.value(100,9999);
+    created_at := sysdate;
+
+    insert into employee values (id, email, first_name, last_name, created_at);
+
+    RETURN(id);
+  COMMIT;
+END;
 /
